16cb9404d244e5c8ded4cb0f4a56f40c9d8b45bc
Haidar <yellowtape47@gmail.com>
2025-07-07 18:24:51 +0300

    FE Done for now

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -2,16 +2,17 @@ import { Link, Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
-	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-			}}>
-			{/* <Redirect href={"/screens/ProfileScreen"} /> */}
-			<Redirect href="/(tabs)/home" />
-		</View>
-	);
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            {/* <Redirect href={"/screens/ProfileScreen"} /> */}
+            <Redirect href="/(tabs)/home/pindex" />
+        </View>
+    );
 }
 //
